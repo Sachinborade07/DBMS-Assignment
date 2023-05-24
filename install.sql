91ec155f0b8a422396dc9a49c6986e894e7350c5
Sachinborade07 <123536691+Sachinborade07@users.noreply.github.com>
2023-05-24 23:22:18 +0530

    Add files via upload

---
+++ install.sql
@@ -0,0 +1,11 @@
+//How to download Mysql on Ubuntu virtual box
+
+sachin@sachin-VirtualBox:~$ sudo apt install mysql-server
+[sudo] password for sachin: 
+Reading package lists... Done
+Building dependency tree... Done
+Reading state information... Done
+mysql-server is already the newest version (8.0.33-0ubuntu0.22.04.2).
+0 upgraded, 0 newly installed, 0 to remove and 232 not upgraded.
+sachin@sachin-VirtualBox:~$ 
+
